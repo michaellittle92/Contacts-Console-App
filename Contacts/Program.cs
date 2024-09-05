2d5f0a3fe477660c92840ff73c327444936b1d35
Michael <michaellittle92@gmail.com>
2024-09-05 22:03:35 +1000

    Basic Create and Read testing.

--- Contacts/Program.cs
+++ Contacts/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Contacts;
+﻿using System.Net.Sockets;
+
+namespace Contacts;
 
 class Program
 {
@@ -9,13 +11,35 @@ class Program
         //Create class with Name, Phone number and email.
         // create object/class with hardcoded values, output contact to console. 
 
-        Contact con = new Contact("Michael", "03 9020 5961", "michaellittle92@gmail.com");
-        con.Print();
+       // Contact con = new Contact("Michael", "03 9020 5961", "michaellittle92@gmail.com");
+       // con.Print();
 
         //2. Create and Read 
         //----------
         // Let user input contact details, store values in list. 
         //Output all contact details to console, using a method (*I think it should be in the contact class???*). Make sure formatting is good.
+
+        List<Contact> contacts = new List<Contact>();
+
+        for (int i = 0; i < 2; i++)
+        {
+            Console.WriteLine("Add Contact");
+            Console.WriteLine("----------");
+            Console.WriteLine("Enter Name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter Phone Number:");
+            string phoneNumber = Console.ReadLine();
+            Console.WriteLine("Enter Email:");
+            string email = Console.ReadLine();
+            Contact cont = new Contact(name, phoneNumber, email);
+            
+            contacts.Add(cont);
+        }
+
+        foreach (Contact c in contacts)
+        {
+            c.Print();
+        }
 
         //3. File Handling
         //----------
